6ef827db3198a33acee8e60eda16baa28fe2ebbe
Manoj Shrestha <mshrestha@Deerwalks-MacBook-Pro.local>
2017-11-08 18:32:04 +0545

    All classes added and Demo project fixed.

--- Example/MSFloatingButton/ViewController.swift
+++ Example/MSFloatingButton/ViewController.swift
@@ -1,24 +1,61 @@
 //
 //  ViewController.swift
-//  MSFloatingButton
+//  MSFloatingButton1
 //
-//  Created by manojshrestha on 11/08/2017.
-//  Copyright (c) 2017 manojshrestha. All rights reserved.
+//  Created by Manoj Shrestha on 7/12/17.
+//  Copyright © 2017 Manoj Shrestha. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var btnAdd: UIButton!
+    
+    let btnDictionary = [["title":"GIF","icon":"icoGif","backgroundColor":UIColor(red: 246.0/255.0, green: 219.0/255.0, blue: 74.0/255.0, alpha: 1.0)],
+                         ["title":"Link","icon":"icoLink","backgroundColor":UIColor(red: 90.0/255.0, green: 187.0/255.0, blue: 139.0/255.0, alpha: 1.0)],
+                         ["title":"Chat","icon":"icoChat","backgroundColor":UIColor(red: 85.0/255.0, green: 159.0/255.0, blue: 202.0/255.0, alpha: 1.0)],
+                         ["title":"Audio","icon":"icoAudio","backgroundColor":UIColor(red: 166.0/255.0, green: 127.0/255.0, blue: 192.0/255.0, alpha: 1.0)],
+                         ["title":"Video","icon":"icoVideo","backgroundColor":UIColor(red: 155.0/255.0, green: 163.0/255.0, blue: 174.0/255.0, alpha: 1.0)],
+                         ["title":"Text","icon":"icoText","backgroundColor":UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 1.0)],
+                         ["title":"Photo","icon":"icoPhoto","backgroundColor":UIColor(red: 215.0/255.0, green: 95.0/255.0, blue: 69.0/255.0, alpha: 1.0)]]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.btnAdd.layer.cornerRadius = self.btnAdd.frame.height/2
+        let bgColor = UIColor(red: 240.0/255.0, green: 152.0/255.0, blue: 60.0/255.0, alpha: 1.0)
+        self.btnAdd.backgroundColor = bgColor
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func btnClicked(_ sender: UIButton)
+    {
+        let floatingCollectionVC = FloatingButtonCollectionViewController()
+        let bgColor = UIColor(red: 55.0/255.0, green: 70.0/255.0, blue: 92.0/255.0, alpha: 1.0)
+        floatingCollectionVC.view.backgroundColor = bgColor
+        for i in 0...(btnDictionary.count - 1) {
+            let fbtn = MSFloatingBtnWithLabel()
+            fbtn.floatingButtonColor = btnDictionary[i]["backgroundColor"] as! UIColor
+            fbtn.titleColor = UIColor.black
+            fbtn.title = btnDictionary[i]["title"] as! String
+            fbtn.titleColor = UIColor.white
+            fbtn.handler = {fbtn in print("clicked \(fbtn.title)")}
+            fbtn.icon = UIImage(named: btnDictionary[i]["icon"] as! String)
+            floatingCollectionVC.addFloatingButton(fbtn)
+        }
+        
+        
+        self.present(floatingCollectionVC, animated: true) {
+            
+        }
+    }
+    
 }
 
