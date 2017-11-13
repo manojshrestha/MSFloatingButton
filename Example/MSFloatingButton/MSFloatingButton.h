28041cfe83e5c7a1103225f510d82ff6ef210ec9
Manoj Shrestha <mshrestha@Deerwalks-MacBook-Pro.local>
2017-11-13 15:51:50 +0545

    Carthage added

---
+++ Example/MSFloatingButton/MSFloatingButton.h
@@ -0,0 +1,19 @@
+//
+//  MSFloatingButton.h
+//  MSFloatingButton
+//
+//  Created by Manoj Shrestha on 11/13/17.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MSFloatingButton.
+FOUNDATION_EXPORT double MSFloatingButtonVersionNumber;
+
+//! Project version string for MSFloatingButton.
+FOUNDATION_EXPORT const unsigned char MSFloatingButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MSFloatingButton/PublicHeader.h>
+
+
